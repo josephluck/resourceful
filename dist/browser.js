9065f94c995701e56d0f944d67fdf60c6e8884b8
Patrick Kunka <patrick@wearecolony.com>
2017-02-01 17:02:14 +0000

    split up browser and node entry points

---
+++ dist/browser.js
@@ -0,0 +1,31 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _ConfigBase = require('./ConfigBase');
+
+var _ConfigBase2 = _interopRequireDefault(_ConfigBase);
+
+var _IResource = require('./IResource');
+
+var _IResource2 = _interopRequireDefault(_IResource);
+
+var _ResourceBase = require('./ResourceBase');
+
+var _ResourceBase2 = _interopRequireDefault(_ResourceBase);
+
+var _ResourceXhr = require('./ResourceXhr');
+
+var _ResourceXhr2 = _interopRequireDefault(_ResourceXhr);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+    ConfigBase: _ConfigBase2.default,
+    IResource: _IResource2.default,
+    ResourceBase: _ResourceBase2.default,
+    ResourceXhr: _ResourceXhr2.default
+};
+//# sourceMappingURL=browser.js.map
--- src/browser.js
+++ src/browser.js
@@ -1,13 +1,11 @@
 import ConfigBase   from './ConfigBase';
 import IResource    from './IResource';
 import ResourceBase from './ResourceBase';
-import ResourceFs   from './ResourceFs';
-import ResourceXhr  from './ResourceFs';
+import ResourceXhr  from './ResourceXhr';
 
 export default {
     ConfigBase,
     IResource,
     ResourceBase,
-    ResourceFs,
     ResourceXhr
 };
